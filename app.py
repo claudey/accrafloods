8dc320f7b4b6f53aa5597b4e563aa82b9422fb45
Claude Ayitey <clauditey@gmail.com>
2015-06-04 15:18:38 +0000

    first commit, just numbers to call

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import (
+    Flask,
+    url_for,
+    render_template,
+    redirect,
+    request,
+    flash
+    )
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
